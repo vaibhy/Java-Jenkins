5416b04467d7fed25a40cd3c0969041a6ac90546
Vaibhav Yergude <88188424+vaibhy@users.noreply.github.com>
2023-11-08 12:40:09 +0530

    only java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public static void main(String[] args) {
     System.out.println("Hi this is java program");
     System.out.println("After adding");
     System.out.println("Addeed one more file");
+    System.out.println("try with only java");
     
 }
 }
