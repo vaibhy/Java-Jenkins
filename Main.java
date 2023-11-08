c6043803ab3c37e7996e78c105b300b6fc578f7f
Vaibhav Yergude <88188424+vaibhy@users.noreply.github.com>
2023-11-08 12:55:07 +0530

    ok stu

--- Main.java
+++ Main.java
@@ -4,6 +4,10 @@ public static void main(String[] args) {
     System.out.println("After adding");
     System.out.println("Addeed one more file");
     System.out.println("try with only java");
+
+    Student stu=new Student();
+    stu.callStu();
+
     
 }
 }
--- Student.java
+++ Student.java
@@ -3,5 +3,9 @@ public class Student {
     public static void main(String[] args) {
         System.out.println("Student");
     }
+
+    public void callStu(){
+        System.out.println("Hi I am in student");
+    }
     
 }
